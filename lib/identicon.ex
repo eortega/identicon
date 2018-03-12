f478632a7ec88c453ea33228ad961356c409f7e5
eortega <eortegamartinez@gmail.com>
2018-03-11 21:31:59 -0600

    First commit

---
+++ lib/identicon.ex
@@ -0,0 +1,20 @@
+defmodule Identicon do
+  @moduledoc """
+  Documentation for Identicon.
+  """
+
+  @doc """
+  main
+
+  """
+
+  def main(input) do
+    input
+    |> hash_input
+  end
+
+  def hash_input(input) do
+    :crypto.hash(:md5, input)
+    |> :binary.bin_to_list
+  end
+end
